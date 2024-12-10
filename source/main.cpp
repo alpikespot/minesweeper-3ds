6608727c7c93a1335a296fc447752a2bc9ab7bb3
alpikespot <antonl1kespotato@gmail.com>
2024-12-10 20:46:28 +0100

    faster movement while holding button

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
+#include <chrono>
 #define BOTTOM_SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 #define TOP_SCREEN_WIDTH  400
@@ -12,6 +13,7 @@
 #define D_UP 6
 #define D_RIGHT 4
 #define D_LEFT 5
+using namespace std;
 int main(int argc, char* argv[])
 {
 	// Init libs
@@ -33,8 +35,15 @@ int main(int argc, char* argv[])
 	u32 clr;
 	int xpos = 0;
 	int ypos = 0;
+	float oldTime; float currentTime; float deltaTime;
+	TickCounter tick;
+	consoleInit(GFX_TOP, NULL);
+	int ticks=0;
+	float bPressT[32] = {0};
+	bool bPressed[32] = {false};
 	while (aptMainLoop())
 	{
+
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START) break; // break in order to return to hbmenu
@@ -43,36 +52,45 @@ int main(int argc, char* argv[])
 
 		u32 kHeld = hidKeysHeld();
 		u32 kUp = hidKeysUp();
-		
-		if (kDown){
-			if (kDown & BIT(D_UP)){ 
-				ypos -= 1;
+		for (int i=0;i<32;i++){
+			if ((kHeld) & BIT(i)){
+				bPressT[i] += 1;
 			}
-			else if (kDown & BIT(D_DOWN)){
-				ypos += 1;
-			}
-			else if (kDown & BIT(D_RIGHT)){
-				xpos += 1;
-			}
-			else if (kDown & BIT(D_LEFT)){
-				xpos -= 1;
+			else {
+				bPressT[i] = 0;
 			}
 		}
 
+			if ((kDown & BIT(D_UP)) || (bPressT[D_UP]>40 && ticks%2==0)){ 
+				ypos -= 1;
+			}
+			else if ((kDown & BIT(D_DOWN)) || (bPressT[D_DOWN]>40 && ticks%2==0)){
+				ypos += 1;
+			}
+			else if (((kDown & BIT(D_RIGHT)) || (kDown & BIT(8))) || (bPressT[D_RIGHT]>40 && ticks%2==0)){
+				xpos += 1;
+			}
+			else if (((kDown & BIT(D_LEFT))|| (kDown & BIT(9))) || (bPressT[D_LEFT]>40 && ticks%2==0)){
+				xpos -= 1;
+			}
+			xpos = C2D_Clamp(xpos, 0, int(BOTTOM_SCREEN_WIDTH/tSize)-1);
+			ypos = C2D_Clamp(ypos, 0, int(SCREEN_HEIGHT/tSize)-1);
+
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 			//--Draw TOP Screen--
+			printf("%f \n", bPressT[D_DOWN]);
+			/*
 			C2D_TargetClear(top, clrClear);
 			C2D_SceneBegin(top);
-			C2D_DrawRectSolid(xpos*tSize+1, ypos*tSize+1,0, tSize-2, tSize-2, clrRec1);
+			C2D_DrawRectSolid(xpos*tSize+1, ypos*tSize+1,0, tSize-2, tSize-2, clrRec1);*/
 
 			//--Draw BOTTOM screen--
 			C2D_TargetClear(bottom, clrClear);
 			C2D_SceneBegin(bottom);
 			C2D_DrawRectSolid(1, 1, 0, tSize-2, tSize-2, clrRec1);
-
 			for (int y=0;y<SCREEN_HEIGHT;y=y+tSize){ for (int x=0;x<BOTTOM_SCREEN_WIDTH;x=x+tSize){
 
-					if (int(x/tSize) == int(touch.px/tSize) && int(y/tSize) == int(touch.py/tSize) && (kDown & BIT(20)))
+					if (int(x/tSize) == int(xpos) && int(y/tSize) == int(ypos)) //&& (kDown & BIT(20)))
 					{clr = clrRecPress;}
 					else{clr = clrRec1;}
 					C2D_DrawRectSolid(x+1, y+1, 0, tSize-2, tSize-2, clr);
@@ -82,6 +100,7 @@ int main(int argc, char* argv[])
 			//printf("\x1b[2;0H%03d; %03d", touch.px, touch.py);
 
 		C3D_FrameEnd(0);
+		ticks++;
 
 		
 	}
